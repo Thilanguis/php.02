1a11876a034a9519e12273298b8726ba4122d230
GABRIEL GRANADO TAVARES <GABRIEL.TAVARES@csi.aneas.msft>
2019-03-21 21:39:30 -0300

    Começando a função de busca no banco

---
+++ consulta.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Amazon</title>
+    
+    <?php include_once 'head.php'; ?>
+    
+</head>
+<body>
+    
+    <div class="container">
+    <h3>Consutar Contatos!</h3>
+    <form action="" method="get" style="width: 500px;">
+    <label>Nome</label>
+    <div class="form-group">
+    <input type="text" name="nome" class="form-control">
+    <input type="submit" value="Buscar" class="btn btn-primary">
+    </div>
+    </form>
+    </div>
+    
+    
+</body>
+</html>
--- gravar.php
+++ gravar.php
@@ -1,6 +1,6 @@
 <?php 
 
-
+ include_once 'head.php';
 
 $nome        = $_POST["nome"];
 $email       = $_POST["email"];
@@ -11,7 +11,7 @@ echo "nome".$nome;
 echo "<br>email".$email;
 echo "<br>estadoCivil".$estadoCivil;
 echo "<br>sexo".$sexo;
-
+ 
 include_once 'conexao.php';
 
 $sql = "insert into contatos values ('null', '".$nome."', '".$email."', '".$estadoCivil."', '".$sexo."')";
@@ -21,10 +21,15 @@ $result = mysqli_query ($con,$sql);
 if ($result)
 {
     echo "Dados gravados com sucesso";
+    ?> <br> <a href="index.php"><button class="btn btn-primary">Ok</button></a> <?php
 }
 
 else
 {
    echo "Erro ao gravar no banco"; 
+   echo "Erro:".mysqli_error($con);
 }
+
+mysqli_close($con);
+    
 ?>
